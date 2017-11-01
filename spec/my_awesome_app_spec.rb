7642dbb2045d91a2c90ba04a59a2cec11df526ff
Jessica Stokes <hello@jessicastokes.net>
2017-11-02 12:15:12 +1300

    Adapt spec to use a custom matcher

--- spec/my_awesome_app_spec.rb
+++ spec/my_awesome_app_spec.rb
@@ -1,9 +1,27 @@
 require 'my_awesome_app'
 
+RSpec::Matchers.define :have_id do |id|
+  # oh boy / adapted from <https://stackoverflow.com/a/16681085>
+  def deep_find(key, value, object = self, found = nil)
+    if object.respond_to?(:key?) && object.key?(key) && object[key] == value
+      object[key]
+    elsif object.is_a? Enumerable
+      object.find { |*a| found = deep_find(key, value, a.last) }
+      found
+    end
+  end
+
+  match do |data|
+    deep_find(:id, id, data).nil? == false
+  end
+end
+
 RSpec.describe 'MyAwesomeApp' do
   it 'returns data with an ID' do
-    expect(MyAwesomeApp.get(:index)).to a_hash_including(
-      some_models: array_including(hash_including(id: 1))
-    )
+    expect(MyAwesomeApp.get(:index)).to have_id(1)
+  end
+
+  it 'returns data with an ID' do
+    expect(MyAwesomeApp.get(:show, id: 1)).to have_id(1)
   end
 end
